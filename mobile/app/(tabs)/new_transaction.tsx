af298f4b4d79e85db89ea7ba918d40b79f957213
Kaleb Mesfin <kalemesfin12@gmail.com>
2023-07-12 23:35:07 +0300

    react native project setup

---
+++ mobile/app/(tabs)/new_transaction.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const new_transaction = (props: Props) => {
+  return (
+    <View>
+      <Text>new_transaction</Text>
+    </View>
+  );
+};
+
+export default new_transaction;
+
+const styles = StyleSheet.create({});
