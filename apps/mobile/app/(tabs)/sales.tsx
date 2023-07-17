2925c467e91241cede65aab2c5e129a923441988
Kaleb Mesfin <kalemesfin12@gmail.com>
2023-07-18 01:03:08 +0300

    feat(mobile): implement searchbar and sales tab

--- apps/mobile/app/(tabs)/sales.tsx
+++ apps/mobile/app/(tabs)/sales.tsx
@@ -1,9 +1,60 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
 import Colors from "../../constants/Colors";
+import { Entypo } from "@expo/vector-icons";
 
 type Props = {};
+
+const DATA = [
+  {
+    id: "1",
+    name: "Egg",
+    imageSrc: require("../../assets/icons/categories/egg.png"),
+  },
+  {
+    id: "2",
+    name: "Milk",
+    imageSrc: require("../../assets/icons/categories/milk.png"),
+  },
+  {
+    id: "3",
+    name: "Biscuit",
+    imageSrc: require("../../assets/icons/categories/biscuit.png"),
+  },
+  {
+    id: "4",
+    name: "Oil",
+    imageSrc: require("../../assets/icons/categories/oil.png"),
+  },
+  {
+    id: "5",
+    name: "Soft",
+    imageSrc: require("../../assets/icons/categories/soft.png"),
+  },
+  {
+    id: "6",
+    name: "Water",
+    imageSrc: require("../../assets/icons/categories/water.png"),
+  },
+  {
+    id: "7",
+    name: "Soft Drink",
+    imageSrc: require("../../assets/icons/categories/soft_drink.png"),
+  },
+  {
+    id: "8",
+    name: "Milk",
+    imageSrc: require("../../assets/icons/categories/milk.png"),
+  },
+];
 
 const sales = (props: Props) => {
   return (
@@ -15,10 +66,108 @@ const sales = (props: Props) => {
           },
         }}
       />
+      <View style={styles.container}>
+        <View
+          style={{
+            backgroundColor: Colors.light.background,
+            width: "100%",
+          }}
+        >
+          <FlatList
+            data={DATA}
+            ListHeaderComponent={
+              <Text
+                style={{
+                  marginLeft: 8,
+                  color: "#828282",
+                  textTransform: "uppercase",
+                }}
+              >
+                Sales
+              </Text>
+            }
+            renderItem={({ item, index }) => (
+              <TouchableOpacity
+                style={{
+                  backgroundColor: "#FFF",
+                  marginVertical: 4,
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  width: "100%",
+                  padding: 16,
+                  paddingHorizontal: 18,
+                  borderRadius: 10,
+                }}
+                onPress={() => {
+                  router.push("/salesDetail");
+                }}
+              >
+                <View>
+                  <Text
+                    style={{
+                      fontFamily: "InterSemiBold",
+                      fontSize: 18,
+                      color: "#4F4F4F",
+                    }}
+                  >
+                    Jul 07, 2023
+                  </Text>
+                  <Text
+                    style={{
+                      fontFamily: "InterRegular",
+                      fontSize: 14,
+                      color: "#80848A",
+                    }}
+                  >
+                    11:51:26
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    gap: 12,
+                    alignItems: "center",
+                  }}
+                >
+                  <View>
+                    <Text
+                      style={{
+                        fontFamily: "InterSemiBold",
+                        fontSize: 18,
+                        color: "#4F4F4F",
+                      }}
+                    >
+                      ETB 1165
+                    </Text>
+                    <Text
+                      style={{
+                        fontFamily: "InterRegular",
+                        fontSize: 14,
+                        color: "#80848A",
+                        textAlign: "right",
+                      }}
+                    >
+                      6 Items
+                    </Text>
+                  </View>
+                  <Entypo name="chevron-right" size={24} color="#4F4F4F" />
+                </View>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default sales;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: Colors.light.background,
+  },
+});
